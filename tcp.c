74a389cbca51d63eb21b71d2214420dac9b01a09
jerry123456123456 <songqy2004@qq.com>
2024-04-13 12:19:38 +0800

    0

--- tcp.c
+++ tcp.c
@@ -1,99 +1,152 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
 #include <sys/epoll.h>
-#include<fcntl.h>
-#include<errno.h>
-#include <ctype.h>
+#include <errno.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <fcntl.h>
 
-#define BUFFER_LENGTH   5
-#define EPOLL_SIZE      1024
 
-int main(int argc, char *argv[]) {
+#define MAX_BUFFER		128
+#define MAX_EPOLLSIZE	(384*1024)
+#define MAX_PORT		100
 
-    if (argc < 2) {
-        printf("Param Error\n");
-        return -1;
-    }
+#define TIME_SUB_MS(tv1, tv2)  ((tv1.tv_sec - tv2.tv_sec) * 1000 + (tv1.tv_usec - tv2.tv_usec) / 1000)
 
-    int port = atoi(argv[1]);
+int isContinue = 0;
 
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+static int ntySetNonblock(int fd) {
+	int flags;
 
-    struct sockaddr_in addr; //设置ipv4地址结构用于指定套接字绑定的地址和端口
-    memset(&addr, 0, sizeof(struct sockaddr_in));
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(port);
-    addr.sin_addr.s_addr = INADDR_ANY;
-    //addr.sin_addr.s_addr = inet_addr("192.168.186.138");	
+	flags = fcntl(fd, F_GETFL, 0);
+	if (flags < 0) return flags;
+	flags |= O_NONBLOCK;
+	if (fcntl(fd, F_SETFL, flags) < 0) return -1;
+	return 0;
+}
 
-    if (bind(sockfd, (struct sockaddr*)&addr, sizeof(struct sockaddr_in)) < 0) {  //绑定
-        perror("bind");
-        return 2;
-    }
+static int ntySetReUseAddr(int fd) {
+	int reuse = 1;
+	return setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, (char *)&reuse, sizeof(reuse));
+}
 
-    if (listen(sockfd, 5) < 0) {
-        perror("listen");
-        return 3;
-    }
 
-    int epfd = epoll_create(1);
-    struct epoll_event events[EPOLL_SIZE];
 
-    struct epoll_event ev;
-    ev.events = EPOLLIN;
-    ev.data.fd = sockfd;
-    epoll_ctl(epfd, EPOLL_CTL_ADD, sockfd, &ev);
+int main(int argc, char **argv) {
+	if (argc <= 2) {
+		printf("Usage: %s ip port\n", argv[0]);
+		exit(0);
+	}
 
-    while (1) {
-        int nready = epoll_wait(epfd, events, EPOLL_SIZE, -1);
-        if (nready == -1) continue;
+	const char *ip = argv[1];
+	int port = atoi(argv[2]);
+	int connections = 0;
+	char buffer[128] = {0};
+	int i = 0, index = 0;
 
-        for (int i = 0; i < nready; i++) {
-            if (events[i].data.fd == sockfd) {
-                struct sockaddr_in client_addr;
-                memset(&client_addr, 0, sizeof(struct sockaddr_in));
-                socklen_t client_len = sizeof(client_addr);
-                int clientfd = accept(sockfd, (struct sockaddr*)&client_addr, &client_len);
+	struct epoll_event events[MAX_EPOLLSIZE];
+	
+	int epoll_fd = epoll_create(MAX_EPOLLSIZE);
+	
+	strcpy(buffer, " Data From MulClient\n");
+		
+	struct sockaddr_in addr;
+	memset(&addr, 0, sizeof(struct sockaddr_in));
+	
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = inet_addr(ip);
 
-		ev.events = EPOLLIN | EPOLLET;
-		ev.data.fd = clientfd;
-                epoll_ctl(epfd, EPOLL_CTL_ADD, clientfd, &ev);
+	struct timeval tv_begin;
+	gettimeofday(&tv_begin, NULL);
 
-                // 设置新连接的套接字为非阻塞模式
-                int flags = fcntl(clientfd, F_GETFL, 0);
-                fcntl(clientfd, F_SETFL, flags | O_NONBLOCK);
-	    } else {
-                int clientfd = events[i].data.fd;
-		while(1){
-			char buffer[BUFFER_LENGTH];
-                	memset(&buffer,0,sizeof(buffer));
-               		int len = recv(clientfd, buffer, BUFFER_LENGTH, 0);
-                	if (len < 0) {
-				if(errno==EAGAIN){
-					printf("数据已经接收完毕...\n");
-					break;
-				}else{
-					close(clientfd);
-                                	epoll_ctl(epfd, EPOLL_CTL_DEL, clientfd, NULL);
-					exit(1);
-				}
-                	} else if (len == 0) { // disconnect
-                    		printf("客户端已经断开连接!  \n");
-				close(clientfd);
-                    		epoll_ctl(epfd, EPOLL_CTL_DEL, clientfd, NULL);
-                	} else {
-				printf("Recv: %s, %d byte(s)\n", buffer, len);
-                    		send(clientfd,buffer,BUFFER_LENGTH,0);	
-			
-                	}
+	while (1) {
+		if (++index >= MAX_PORT) index = 0;
+		
+		struct epoll_event ev;
+		int sockfd = 0;
+
+		if (connections < 340000 && !isContinue) {
+			sockfd = socket(AF_INET, SOCK_STREAM, 0);
+			if (sockfd == -1) {
+				perror("socket");
+				goto err;
+			}
+
+			//ntySetReUseAddr(sockfd);
+			addr.sin_port = htons(port+index);
+
+			if (connect(sockfd, (struct sockaddr*)&addr, sizeof(struct sockaddr_in)) < 0) {
+				perror("connect");
+				goto err;
+			}
+			ntySetNonblock(sockfd);
+			ntySetReUseAddr(sockfd);
+
+			sprintf(buffer, "Hello Server: client --> %d\n", connections);
+			send(sockfd, buffer, strlen(buffer), 0);
+
+			ev.data.fd = sockfd;
+			ev.events = EPOLLIN | EPOLLOUT;
+			epoll_ctl(epoll_fd, EPOLL_CTL_ADD, sockfd, &ev);
+		
+			connections ++;
 		}
-            }
-        }
-    }
+		//connections ++;
+		if (connections % 1000 == 999 || connections >= 340000) {
+			struct timeval tv_cur;
+			memcpy(&tv_cur, &tv_begin, sizeof(struct timeval));
+			
+			gettimeofday(&tv_begin, NULL);
 
-    return 0;
+			int time_used = TIME_SUB_MS(tv_begin, tv_cur);
+			printf("connections: %d, sockfd:%d, time_used:%d\n", connections, sockfd, time_used);
+
+			int nfds = epoll_wait(epoll_fd, events, connections, 100);
+			for (i = 0;i < nfds;i ++) {
+				int clientfd = events[i].data.fd;
+
+				if (events[i].events & EPOLLOUT) {
+					sprintf(buffer, "data from %d\n", clientfd);
+					send(sockfd, buffer, strlen(buffer), 0);
+				} else if (events[i].events & EPOLLIN) {
+					char rBuffer[MAX_BUFFER] = {0};				
+					ssize_t length = recv(sockfd, rBuffer, MAX_BUFFER, 0);
+					if (length > 0) {
+						printf(" RecvBuffer:%s\n", rBuffer);
+
+						if (!strcmp(rBuffer, "quit")) {
+							isContinue = 0;
+						}
+						
+					} else if (length == 0) {
+						printf(" Disconnect clientfd:%d\n", clientfd);
+						connections --;
+						close(clientfd);
+					} else {
+						if (errno == EINTR) continue;
+
+						printf(" Error clientfd:%d, errno:%d\n", clientfd, errno);
+						close(clientfd);
+					}
+				} else {
+					printf(" clientfd:%d, errno:%d\n", clientfd, errno);
+					close(clientfd);
+				}
+			}
+		}
+
+		usleep(1 * 1000);
+	}
+
+	return 0;
+
+err:
+	printf("error : %s\n", strerror(errno));
+	return 0;
+	
 }
